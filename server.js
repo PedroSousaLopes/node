53658cdb02426306a5bf0b83e4b19ccf2ff5a762
Pedro Sousa Lopes <pedrosousa4304@gmail.com>
2025-01-25 09:54:08 -0300

    Primeiro commit

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+const path = require('path');
+const mongoose = require ('mongoose');
+
+mongoose.connect(process.env.CONECTIONSTRING, {
+  useNewUrlParser: true, // Usa o novo parser de URLs (recomendado)
+  useUnifiedTopology: true, // Usa o novo mecanismo de gerenciamento de conexões
+}).then(()=>
+  console.log('conecetei a base de dados'),
+  app.emit("Pronto"));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+app.use(routes);
+
+app.on("Pronto",()=>{
+  app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
+  });
+})
